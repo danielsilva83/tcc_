38cd5fe0226179a518aaad5062068a9d31419416
Daniel Kognita <danielsilva.2013@alunos.utfpr.edu.br>
2022-05-29 21:22:35 -0300

    req

--- myproject/settings.py
+++ myproject/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
