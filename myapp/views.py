bebe225bb4ed0579c9ac1e9271c7eeaf79b394b8
Daniel Kognita <danielsilva.2013@alunos.utfpr.edu.br>
2022-05-30 23:46:46 -0300

    req

--- myapp/views.py
+++ myapp/views.py
@@ -44,8 +44,8 @@ def my_result(request):
 
     form = DocumentForm()  
     documents = Document.objects.all()
-    nvezes = request.GET.get('nvezes','')
-    #nvezes = int(nvezes)
+    nvezes_ = request.GET.get('nvezes','')
+    nvezes = int(nvezes_)
     #2 #nvezes = numero de repetições do experimento para cada um dos conjuntos de dados
     nreduce = [0.1,0.12] #nreduce = lista com as porcentagens de redução para cada conjunto de dados
     list_df_ft = []
