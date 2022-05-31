d2e6f9e06af3663a05ddb2c37405192c7505d448
Daniel Kognita <danielsilva.2013@alunos.utfpr.edu.br>
2022-05-30 21:21:23 -0300

    req

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,10 +28,10 @@ SECRET_KEY = '@2tcr%p+je0h#+ujlqu*s%ax!$s2%^w9rh%=eg2jv270o3p0+b'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['tccgraf.herokuapp.com' , 'tccgraf.herokuapp.com']
-
+ALLOWED_HOSTS = ['tccgraf.herokuapp.com' , 'tccgraf.herokuapp.com',
+                 'tccgraf.herokuapp.com/result']
 # Application definition
 
 INSTALLED_APPS = [
