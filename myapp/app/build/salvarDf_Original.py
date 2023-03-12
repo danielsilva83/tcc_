440e2c60184c52fd7a4fafca1808666ef806ad90
Daniel <daniel.goncalves@kognita.ai>
2023-03-12 19:15:13 -0300

    fix: fix file

--- myapp/app/build/salvarDf_Original.py
+++ myapp/app/build/salvarDf_Original.py
@@ -6,7 +6,7 @@ def salvarDf_Original (dataframeOriginal,file_name, name_file ):
     name_file = name_file.replace("/", "_")
     name_file = name_file.replace("media", "")
     name_file = name_file.replace("documents", "")
-    file_name_ = '../media/documents/exports/'+file_name+name_file+'_original_'+'.csv'
+    file_name_ = '../../media/documents/exports/'+file_name+name_file+'_original_'+'.csv'
     file_name_1 = 'media/documents/exports/'+file_name+name_file+'_original_'+'.csv'
     #print(file_name_+'_original_'+'.csv')
     dataframeOriginal = pd.DataFrame(dataframeOriginal)
